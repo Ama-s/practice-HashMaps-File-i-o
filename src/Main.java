833f0b7e3c661574180dea301c7391ed5de8a5e5
Deborah Arku <arkudeborah123@gmail.com>
2024-07-22 09:22:31 +0100

    two ways of executing file i/o

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,7 @@ public class Main {
             }
         } catch (EOFException e) {
             System.out.println("Reached end of file unexpectedly: " + e.getMessage());
+            //exit
         }
     }
 }
